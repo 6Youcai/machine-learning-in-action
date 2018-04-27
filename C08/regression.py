1242ce5c40ee09c3b8b2a1eed952ef79e744a543
youcai <youcai.6@qq.com>
2018-04-27 15:57:04 +0800

    add regression 2

--- C08/regression.py
+++ C08/regression.py
@@ -27,17 +27,54 @@ def stand_regress(xarr, yarr):
     return ws
 
 def lwlr(test_point, xarr, yarr, k = 1.0):
-    pass
+    x_mat = np.mat(xarr)
+    y_mat = np.mat(yarr).T
+    # 局部加权线性回归
+    m = x_mat.shape[0]
+    weights = np.mat(np.eye((m)))
+    for j in range(m):
+        diff_mat = test_point - x_mat[j, :]
+        weights[j, j] = np.exp(diff_mat * diff_mat.T / (-2.0 * k ** 2))
+    xTx = x_mat.T * (weights * x_mat)
+    if np.linalg.det(xTx) == 0:
+        return
+    ws = xTx.I * (x_mat.T * (weights * y_mat))
+    return test_point * ws
 
 def lwlr_test(test_arr, xarr, yarr, k = 1.0):
-    pass
+    m = np.shape(test_arr)[0]
+    y_hat = np.zeros(m)
+    for i in range(m):
+        y_hat[i] = lwlr(test_arr[i], xarr, yarr, k)
+    return y_hat
 
 def rss_error(yarr, yhat_arr):
     # 误差平方和
     return ((yarr - yhat_arr)**2).sum()
 
-# 岭回归
+def ridge_regress(x_mat, y_mat, lam = 0.2):
+    # 岭回归
+    xTx = x_mat.T * x_mat
+    denom = xTx + np.eye(x_mat.shape[1]) * lam
+    if np.linalg.det(denom) == 0.0:
+        return
+    ws = denom.I * (x_mat.T * y_mat)
+    return ws
+
+def ridge_test(xarr, yarr):
+    x_mat = np.mat(xarr)
+    y_mat = np.mat(yarr).T
+    y_mean = np.mean(y_mat, 0)
+    y_mat = y_mat - y_mean
+    x_mean = np.mean(x_mat, 0)
+    x_var = np.var(x_mat, 0)
+    x_mat = (x_mat - x_mean) / x_var
+    num_test = 30
+    w_mat = np.zeros((num_test, x_mat.shape[1]))
+    for i in range(num_test):
+        ws = ridge_regress(x_mat, y_mat, np.exp(i - 10))
+        w_mat[i: ] = ws.T
+    return w_mat
 
 # lasso
-
-# 
+# 前向逐步回归
